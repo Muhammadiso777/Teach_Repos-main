f0881578cf10497a91b5269cb84ca11cfbbc0c45
Sobitxonov Muhammadiso <sobitxonovmuhammadiso00@mail.ru>
2023-06-14 17:06:16 +0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# 1-dars
+
+Darslik: https://github.com/murodov20/teaching-links
+
+
+Figma: https://www.figma.com/file/s36s62TYEjfDWoRy1vvK9s/Responsive-design-for-learning-(Community)?type=design&node-id=0-1&t=W1SiJbNJVJc32G8h-0
---
+++ js/main.js
@@ -0,0 +1,26 @@
+// $(document).ready(function(){
+//     $(".box").hover(function(){
+//         $(".title",".box-help",".box-date",".box-list").css("color" , "red")
+//     });
+
+
+
+// })
+  
+    //   $(window).resize(function(){
+    //     if ($(window).width() <= 960) {  
+    //   const link5 = $('.link5')
+    //   link5.text('Еще')
+    //   document.querySelector('.link-btn').style.display='none';
+    //   // $('.link-btn').style.display='none';
+    //   }     
+    //   else{
+    //     document.querySelector('.link-btn').style.display='flex';
+    //     const link5 = $('.link5')
+    //     link5.text('Сообщество')
+    //   }
+    
+    //  if($(window).width() <= 768){
+    //   $('.link4').text('')
+    //  }else{
+    //   $('.link4').text('Базы резюме')
